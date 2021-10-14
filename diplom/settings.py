6a3962df4ab00ac177802b9d4c31db63bec3b38a
Dmytro Bezpalov <81474798+terpator@users.noreply.github.com>
2021-10-14 18:20:32 +0300

    Update settings.py

--- diplom/settings.py
+++ diplom/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$61s-ol^7qc7v)o&n+n1%ygomy7lq2mmsvz62b92f&j_x8+21'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
